fd556f11ffd9b47784c63674c867030f1cd1f7a8
KunYuChang <mischiefsub@gmail.com>
2021-10-19 16:57:55 +0800

    Handling POST Requests

--- app.js
+++ app.js
@@ -2,15 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You cna post to this endpoint...');
-// });
+// express.json() : Express 可以解讀 JSON String 轉成 JavaScript Object
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -24,6 +17,27 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // 產生新ID : 抓取最後一筆資料ID + 1
+  const newId = tours[tours.length - 1].id + 1;
+  // 合併 ID & POST過來的資料 (req.body)
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
