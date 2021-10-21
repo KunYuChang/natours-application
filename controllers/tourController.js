ee42266b9cb25e0e026cb7b461e6c12a2218ebd6
KunYuChang <mischiefsub@gmail.com>
2021-10-22 05:48:20 +0800

    api sort limit-fields pagination aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,23 +1,66 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // 1) 創立查詢
     // http://localhost:3000/api/v1/tours?duration[gte]=5&difficulty=easy
     const queryObj = { ...req.query };
 
-    // 初階過濾
+    // 2A) 初階過濾
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 進階過濾
+    // 2B) 進階過濾
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    // MongoDB 依過濾條件尋找資料
-    const query = Tour.find(JSON.parse(queryStr));
+    // 3) MongoDB 依過濾條件尋找資料
+    let query = Tour.find(JSON.parse(queryStr));
 
-    // 2) 執行查詢
+    // 4) 排序
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 5) 限制欄位
+    if (req.query.fields) {
+      // http://localhost:3000/api/v1/tours?fields=name,duration,difficulty,price
+      // - 回傳 name + duration + difficulty + price 的資料 (還有_id一定要回傳)
+      // - 稱為 projecting
+
+      // http://localhost:3000/api/v1/tours?fields=-name,-duration
+      // - 回傳除了 name & duration 以外的資料。
+
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 6) 分頁 Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip > numTours) throw new Error('This page does not exist');
+    }
+
+    // http://localhost:3000/api/v1/tours?page=2&limit=3
+    query = query.skip(skip).limit(limit);
+
+    // 7) 執行查詢
     const tours = await query;
 
     // Mongoose 依過濾條件尋找資料 (作為參考)
@@ -27,7 +70,7 @@ exports.getAllTours = async (req, res) => {
     //   .where('difficulty')
     //   .equlas('easy');
 
-    // 3) 發送回應
+    // 8) 發送回應
     res.status(200).json({
       status: 'success',
       results: tours.length,
