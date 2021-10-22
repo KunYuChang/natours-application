28fb2d55001c0101d345907046edaba137886116
KunYuChang <mischiefsub@gmail.com>
2021-10-22 16:30:47 +0800

    refactoring api features

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # natours-application
+
+一個露營網站
+
+## Architecture
+
+- Classes
+- MVC
+
+## Folder
+
+- routes : 設定路由
+- controllers : 路由函式
+- models : 資料庫綱要
+- public : 靜態檔案
+- utils : 共用函式
+- dev-data : 開發資料
+
+# Format
+
+- Prettier
+- ESLint
+
+## Setting
+
+- config.env
+- .gitignore
+
+## Tech Stack
+
+- HTML
+- CSS
+- JavaScript
+- Node
+- Express
+- MongoDB
+- Mongoose
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -9,66 +10,13 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // 1) 創立查詢
-    // http://localhost:3000/api/v1/tours?duration[gte]=5&difficulty=easy
-    const queryObj = { ...req.query };
-
-    // 2A) 初階過濾
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 2B) 進階過濾
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    // 3) MongoDB 依過濾條件尋找資料
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 4) 排序
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 5) 限制欄位
-    if (req.query.fields) {
-      // http://localhost:3000/api/v1/tours?fields=name,duration,difficulty,price
-      // - 回傳 name + duration + difficulty + price 的資料 (還有_id一定要回傳)
-      // - 稱為 projecting
-
-      // http://localhost:3000/api/v1/tours?fields=-name,-duration
-      // - 回傳除了 name & duration 以外的資料。
-
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 6) 分頁 Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip > numTours) throw new Error('This page does not exist');
-    }
-
-    // http://localhost:3000/api/v1/tours?page=2&limit=3
-    query = query.skip(skip).limit(limit);
-
     // 7) 執行查詢
-    const tours = await query;
-
-    // Mongoose 依過濾條件尋找資料 (作為參考)
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equlas('easy');
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // 8) 發送回應
     res.status(200).json({
