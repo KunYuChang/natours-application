509bec48373a134685819056463651d523a4b14f
KunYuChang <mischiefsub@gmail.com>
2021-10-31 12:33:23 +0800

    nested router

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+
+// POST /tour/234fad4/reviews
+// POST /reviews
+// 都會被導向route('/')
 
 router
   .route('/')
